fffd7389e14a997d2025ba74eadee141ef83f8c1
smeans <smeans@gmail.com>
2013-06-24 13:36:37 -0400

    Initial Commit

---
+++ checkov/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  checkov
+//
+//  Created by Scott Means on 6/24/13.
+//  Copyright (c) 2013 Scott Means. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ checkov/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  checkov
+//
+//  Created by Scott Means on 6/24/13.
+//  Copyright (c) 2013 Scott Means. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
